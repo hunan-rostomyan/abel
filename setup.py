880a4a870e7ec00b9d6db117ba7ba5d3fa58be96
Hunan Rostomyan <hunan131@gmail.com>
2018-06-30 10:53:00 -0700

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='abel',
+    version='0.0.1',
+    description='Machine learning components',
+    url='http://github.com/hunan-rostomyan/abel',
+    author='Hunan Rostomyan',
+    author_email='hunan131@gmail.com',
+    license='BSD 3-Clause',
+    packages=['abel'],
+    zip_safe=False
+)
