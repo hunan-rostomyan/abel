ba8f04d20b328fed5a75d0ede5bf1f3c79df6696
Hunan Rostomyan <hunan131@gmail.com>
2018-06-30 17:41:46 -0700

    Add license entry to setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='abel',
-    version='0.0.4',
+    version='0.0.5',
     author='Hunan Rostomyan',
     author_email='hunan131@gmail.com',
     description='Machine learning components',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/hunan-rostomyan/abel',
+    license='BSD 3-clause',
     packages=setuptools.find_packages(),
     classifiers=(
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
     ),
 )
